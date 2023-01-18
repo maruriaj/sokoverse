d8960a3dc1ba78b468613aefe3ae9518a7cead35
Maruria Joseph <josephmaruria@gmail.com>
2023-01-18 09:59:17 +0300

    Sokoverse

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,10 +1,14 @@
-import { apple, about, google, } from "../assets";
-import styles, { layout } from "../style";
+import { apple, about, google } from '../assets';
+import styles, { layout } from '../style';
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
-      <img src={about} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <img
+        src={about}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
 
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
@@ -14,15 +18,27 @@ const Billing = () => (
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        Sokoverse suits all<br className="sm:block hidden" />  your business needs.
+        Sokoverse suits all
+        <br className="sm:block hidden" /> your business needs.
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Whether you are a big or small business SokoVerse provides digital tools for you to get more customers and increase your sales.Our platforms are highly configurable and our team works closely with your brands team to deliver real results.
+        Whether you're a big or small business SokoVerse provides digital tools
+        for you to get more customers and increase your sales.Our platforms are
+        highly configurable and our team works closely with your brands team to
+        deliver real results.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
-        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+        <img
+          src={apple}
+          alt="google_play"
+          className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google}
+          alt="google_play"
+          className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
+        />
       </div>
     </div>
   </section>
